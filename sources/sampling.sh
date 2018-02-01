775e4dbedf3de702fc633b12178b9c4bb19d4efc
Barrat Pierre <p.barrat@live.fr>
2018-02-01 14:32:34 +0100

    sampling.sh : now deleting graph in MCMC format instead of storing it.

--- sources/sampling.sh
+++ sources/sampling.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 steps=(0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95)
-for s in ${steps[*]}; do
-	echo $s
-done
 
 while read f; do
 	echo ""
@@ -22,6 +19,7 @@ while read f; do
 		mv out_samp* sample_$s.txt
 		et="$(date -u +%s)"
 		elapsed="$(($et-$st))"
+		rm plmInf_$s_"MCMC.txt" 
 		echo "$elapsed seconds"
 	done
 	echo "${steps[*]}" > steps.txt
