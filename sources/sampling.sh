0a039ed3db363080989b1ebb3336cfe6cf1321ec
Barrat Pierre <p.barrat@live.fr>
2018-02-01 16:42:12 +0100

    Fixed bug that graph_MCMC was not deleted.

--- sources/sampling.sh
+++ sources/sampling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-steps=(0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95)
+steps=(0 5 10 15 20 25 30 35 40)
 
 while read f; do
 	echo ""
@@ -19,7 +19,7 @@ while read f; do
 		mv out_samp* sample_$s.txt
 		et="$(date -u +%s)"
 		elapsed="$(($et-$st))"
-		rm plmInf_$s_"MCMC.txt" 
+		rm plmInf_$s"_MCMC.txt" 
 		echo "$elapsed seconds"
 	done
 	echo "${steps[*]}" > steps.txt
