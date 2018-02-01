4ce7571f199b61edcb73bb44e8f0465ea1d1cb49
Barrat Pierre <p.barrat@live.fr>
2018-02-01 12:06:22 +0100

    Working version of sampling.sh

--- sources/sampling.sh
+++ sources/sampling.sh
@@ -6,16 +6,24 @@ for s in ${steps[*]}; do
 done
 
 while read f; do
-	echo $f
-	cd $f_results
+	echo ""
+	echo "Family: $f"
+	cd $f"_results"
 	mkdir -p samples
 	lseq=`head -n 1 msa_numerical.txt |awk '{print NF}'`
 	echo "Length of sequence: $lseq"
 	cd samples
 	for s in ${steps[*]}; do
-		../../sources/graph_mat_to_MCMC ../decimation_results/plmInf_$s_mat.txt plmInf_$s_MCMC.txt $lseq 21
-		../../sources/do_montecarlo -n $lseq -q 21 -t 5000 -m 2000 < plmInf_$s_MCMC.txt 
+		echo "Decimation: $s"
+		st="$(date -u +%s)" 
+		../../sources/graph_mat_to_MCMC ../decimation_results/plmInf_$s"_mat.txt" plmInf_$s"_MCMC.txt" $lseq 21
+		../../sources/do_montecarlo -n $lseq -q 21 -t 5000 -m 5000 -s $s < plmInf_$s"_MCMC.txt" &> /dev/null
 		rm out_energies* 
 		mv out_samp* sample_$s.txt
+		et="$(date -u +%s)"
+		elapsed="$(($et-$st))"
+		echo "$elapsed seconds"
 	done
+	echo "${steps[*]}" > steps.txt
+	echo ""
 done <$1
